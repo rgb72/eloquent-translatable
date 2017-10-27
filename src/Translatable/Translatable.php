9efefe514eee8cf643187d7d1008ab0cdbf7021a
Maetad Suakarasud <pan@rgb72.com>
2017-10-27 16:44:35 +0700

    change vendor name

--- src/Translatable/Translatable.php
+++ src/Translatable/Translatable.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Dimsav\Translatable;
+namespace Rgb72\Translatable;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Query\Builder as QueryBuilder;
-use Dimsav\Translatable\Exception\LocalesNotDefinedException;
+use Rgb72\Translatable\Exception\LocalesNotDefinedException;
 
 trait Translatable {
 
